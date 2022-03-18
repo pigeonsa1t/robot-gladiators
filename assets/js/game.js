76870daee7d72d72dcb1a494c5da3f4ef3247cf7
pigeonsa1t <bowen_samantha@yahoo.com>
2022-03-18 16:32:40 -0400

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -12,6 +12,8 @@ console.log(enemyNames.length);
 console.log(enemyNames[0]);
 console.log(enemyNames[3]);
 
+//FIGHT://
+
 // fight function (now with parameter for enemy's name)
 var fight = function(enemyName) {
   while (playerHealth > 0 && enemyHealth > 0) {
@@ -32,8 +34,9 @@ var fight = function(enemyName) {
         break;
       }
     }
-    // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = Math.max(0, enemyHealth - playerAttack);
+    // generate random damage value based on player's attack power 
+    var damage = randomNumber(playerAttack - 3, playerAttack);
+    enemyHealth = Math.max(0, enemyHealth - damage);
     console.log(
       playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
     );
@@ -51,8 +54,9 @@ var fight = function(enemyName) {
       window.alert(enemyName + ' still has ' + enemyHealth + ' health left.');
     }
 
-    // remove players's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
+    // generate random damage value based on enemy's attack power
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+    playerHealth = Math.max(0, playerHealth - damage);
     console.log(
       enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
     );
@@ -68,6 +72,17 @@ var fight = function(enemyName) {
   }
 };
 
+// RANDOM NUMBER: //
+
+// function to generate a random numeric value
+var randomNumber = function(min,max) {
+  var value = Math.floor(Math.random() * (max -min + 1)) + min  ;
+
+  return value;
+};
+
+// START GAME: //
+
 var startGame = function() {
   // reset player stats
   playerHealth = 100;
@@ -82,7 +97,7 @@ var startGame = function() {
     // pick new enemy to fight based on the index of the enemyNames array
       var pickedEnemyName = enemyNames[i];
       // reset enemyHealth before starting new fight
-      enemyHealth = 50;
+      enemyHealth = randomNumber(40,60);
       // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
       fight(pickedEnemyName);
       // if we're not on the last enemy in the array
@@ -104,6 +119,8 @@ var startGame = function() {
   endGame();
 };
 
+// END GAME: //
+
 // function to end the entire game:
 var endGame = function() {
   // if player is still alive, player wins!
@@ -123,6 +140,8 @@ var playAgainConfirm = window.confirm("Would you like to play again?");
     window.alert("Thank you for playing Robot Gladiators! Come back soon!");
   }
 };
+
+// SHOP: //
 
 var shop = function() {
   var shopOptionPrompt = window.prompt(
